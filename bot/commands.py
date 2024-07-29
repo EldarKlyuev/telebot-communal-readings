a98c584a6ca3b7b7cdbae75715a97f4ad061207e
Eldar Klyuev <e.klyuev@notamedia.com.ru>
2024-07-29 15:52:07 +0300

    feat: add start, test, getmyid commands

--- bot/commands.py
+++ bot/commands.py
@@ -6,6 +6,8 @@ async def setup_bot_command(bot):
 
     bot_command = [
         BotCommand(command='/start', description='Старт'),
+        BotCommand(command='/test', description='Тест работы'),
+        BotCommand(command='/getmyid', description='Получить свой ChatID'),
     ]
 
     await bot.set_my_commands(bot_command)
--- bot/handlers/routers.py
+++ bot/handlers/routers.py
@@ -1,5 +1,39 @@
-import requests
+import datetime
 
 from aiogram import Router, F
+from aiogram import types
+from aiogram.enums.dice_emoji import DiceEmoji
+from aiogram.types import Message
+from aiogram.filters import Command
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+from filters.admin_filter import AdminCreatorFilter
+from filters.chat_type import GroupTypeFilter
 
 router = Router()
+
+
+@router.message(
+    Command(commands=['start']),
+    AdminCreatorFilter(is_admin=True)
+)
+async def cmd_start(message: Message):
+    """Start command"""
+    await message.answer(f'@{message.from_user.username}, привет! Ты хочешь подать показания?')
+
+
+@router.message(
+    Command(commands=["test"])
+)
+async def cmd_test(message: Message):
+    """Проверка работоспособности"""
+    await message.answer(f"@{message.from_user.username}, я работаю!")
+
+
+@router.message(
+    Command(commands=["getmyid"]),
+)
+async def cmd_get_chat_id(message: Message):
+    """Получение свое ChatID"""
+    user_id = message.from_user
+    await message.answer(f"""@{user_id.username}, привет!\nТвой chatId = {user_id.id}""")
