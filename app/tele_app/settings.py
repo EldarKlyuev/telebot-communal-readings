7d9f6baa65036d6b8c34f38a5316417b8806bfa1
Eldar Klyuev <e.s.klyuev@gmail.com>
2024-09-30 10:57:14 +0300

    feat: hide secret

--- app/tele_app/settings.py
+++ app/tele_app/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d)244tu7bt-@6$2ct*4obd!r7bf3h-3-sqaze8_b6b0r-vbh^='
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
