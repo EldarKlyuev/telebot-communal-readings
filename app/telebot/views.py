27166c5585237763caee8093519d390ca6f9bbad
Eldar Klyuev <e.s.klyuev@gmail.com>
2024-09-30 12:47:47 +0300

    feat: add view post

--- app/telebot/views.py
+++ app/telebot/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from telebot.serializers import ZipSerializer
+
+
+class ShkolnyaAPIView(APIView):
+    def post(self, request):
+        serializer = ZipSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        return Response(status=status.HTTP_200_OK)
